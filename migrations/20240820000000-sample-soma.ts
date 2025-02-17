029f31b9bdd88e82c4643c4d7428c7f89f77bcd4
Patrick Edson <pedson@users.noreply.github.com>
2025-02-17 07:08:28 -0500

    Add sample soma location to neuron entry.

---
+++ migrations/20240820000000-sample-soma.ts
@@ -0,0 +1,32 @@
+import {QueryInterface} from "sequelize";
+
+import {NeuronTableName} from "./src/TableNames";
+
+export = {
+    up: async (queryInterface: QueryInterface, Sequelize: any) => {
+        await queryInterface.addColumn(NeuronTableName,
+            "sampleX", {
+                type: Sequelize.DOUBLE,
+                defaultValue: 0
+            },
+        );
+        await queryInterface.addColumn(NeuronTableName,
+            "sampleY", {
+                type: Sequelize.DOUBLE,
+                defaultValue: 0
+            },
+        );
+        await queryInterface.addColumn(NeuronTableName,
+            "sampleZ", {
+                type: Sequelize.DOUBLE,
+                defaultValue: 0
+            },
+        );
+    },
+
+    down: async (queryInterface: QueryInterface, _: any) => {
+        await queryInterface.removeColumn(NeuronTableName, "sampleX");
+        await queryInterface.removeColumn(NeuronTableName, "sampleY");
+        await queryInterface.removeColumn(NeuronTableName, "sampleZ");
+    }
+}
